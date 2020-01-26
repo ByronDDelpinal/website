a49b00952d42b9eb34a2a7aa7630f7353a4f1341
Byron Delpinal <byron@heysparkbox.com>
2020-01-26 10:27:11 -0500

    Initial commit from gatsby: (https://github.com/EmaSuriano/gatsby-starter-mate.git)

---
+++ colors.js
@@ -0,0 +1,16 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#f0e6f6',
+
+  primary: '#003F9B',
+  primaryLight: '#0053CC',
+  primaryDark: '#00265D',
+
+  secondary: '#79D600',
+  secondaryLight: '#89F200',
+  secondaryDark: '#498100',
+
+  tertiary: '#83009A',
+  tertiaryLight: '#AE00CC',
+  tertiaryDark: '#4F005D',
+};
---
+++ manifest-config.js
@@ -0,0 +1,11 @@
+const colors = require('./colors');
+
+module.exports = {
+  name: 'Byron Delpinal Personal Website',
+  short_name: 'Byron Delpinal Website',
+  start_url: '/',
+  background_color: colors.background,
+  theme_color: colors.primary,
+  display: 'minimal-ui',
+  icon: 'media/icon.png',
+};
